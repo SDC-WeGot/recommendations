f63f2505f0c8335f1c1270cb58204ed7ac73c7d3
Henry Yang <henryyang@Henrys-MacBook-Air.local>
2018-03-19 22:04:03 -0700

    Create schemas

---
+++ db/models/pg-restaurant.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS nearby;
+
+CREATE TABLE restaurants (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  place_id INTEGER UNIQUE NOT NULL,
+  google_rating REAL NOT NULL,
+  zagat_food_rating REAL NOT NULL,
+  review_count INT NOT NULL,
+  short_description TEXT NOT NULL,
+  neighborhood TEXT NOT NULL,
+  price_level SMALLINT NOT NULL,
+  type TEXT NOT NULL,
+  location JSONB NOT NULL,
+  photos ARRAY NOT NULL
+);
+
+CREATE TABLE nearby (
+  place_id INTEGER NOT NULL REFERENCES restaurants (place_id),
+  recommended INTEGER NOT NULL REFERENCES restaurants (place_id)
+);
+
+-- terminal command to run the sql file
+-- psql -f pg-resetaurant.sql sagat-sql

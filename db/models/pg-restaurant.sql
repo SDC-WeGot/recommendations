c0981b2d0f532be0d828ecf4fec6e7cd5ae0effa
Henry Yang <contacthenryyang@gmail.com>
2018-03-20 20:24:53 -0700

    Flatten long/lat into own columns; add create database check

--- db/models/pg-restaurant.sql
+++ db/models/pg-restaurant.sql
@@ -1,10 +1,12 @@
+CREATE DATABASE sagat_sql;
+
 DROP TABLE IF EXISTS nearby;
 DROP TABLE IF EXISTS restaurants;
 
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY,
   business_name TEXT NOT NULL,
-  place_id INTEGER UNIQUE NOT NULL,
+  place_id SERIAL UNIQUE NOT NULL,
   google_rating REAL NOT NULL,
   zagat_food_rating REAL NOT NULL,
   review_count INT NOT NULL,
@@ -12,7 +14,8 @@ CREATE TABLE restaurants (
   neighborhood TEXT NOT NULL,
   price_level SMALLINT NOT NULL,
   business_type TEXT NOT NULL,
-  business_location JSONB NOT NULL,
+  longitude REAL NOT NULL,
+  latitude REAL NOT NULL,
   photos TEXT[] NOT NULL
 );
 
@@ -21,5 +24,5 @@ CREATE TABLE nearby (
   recommended INTEGER NOT NULL REFERENCES restaurants (place_id)
 );
 
--- terminal command to run the sql file
--- psql -f pg-restaurant.sql sagat-sql
+-- terminal command to run the sql file, naming which database to do the work in
+-- psql -f pg-restaurant.sql sagat_sql 

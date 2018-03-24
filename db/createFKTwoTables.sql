f5996e239eac3b0849fe6d0bed4e297807d22c0b
Henry Yang <henryyang@MVPMBP.local>
2018-03-24 16:47:46 -0700

    Add create index script and query commands

--- db/createFKTwoTables.sql
+++ db/createFKTwoTables.sql
@@ -6,6 +6,20 @@ ALTER TABLE nearby
 ADD CONSTRAINT fk_nearby_recommended_placeid
 FOREIGN KEY (recommended) REFERENCES restaurants (place_id);
 
+
+CREATE INDEX index_nearby_recommended_placeid 
+ON nearby (recommended);
+
+CREATE INDEX index_nearby_recommended_placeid 
+ON nearby (place_id);
+
+
+
 -- 'ALTER TABLE nearby ADD CONSTRAINT nearby_placeid_placeid FOREIGN KEY (place_id) REFERENCES restaurants (place_id); ALTER TABLE nearby ADD CONSTRAINT nearby_recommended_placeid FOREIGN KEY (recommended) REFERENCES restaurants (place_id);'
 
--- need to go back and remove constraints, readd foreign key constraints
+-- Create index command
+-- 'CREATE INDEX index_nearby_recommended ON nearby (recommended); CREATE INDEX index_nearby_placeid ON nearby (place_id);'
+
+
+-- Query command
+-- SELECT * FROM restaurants INNER JOIN nearby ON restaurants.place_id = nearby.recommended WHERE nearby.place_id = 1;

feed7dab4085c1a1ea763ef770159ad743b828eb
Henry Yang <henryyang@MVPMBP.local>
2018-03-23 17:31:49 -0700

    Add third join table

--- db/models/pg-restaurant.sql
+++ db/models/pg-restaurant.sql
@@ -1,9 +1,11 @@
 DROP TABLE IF EXISTS nearby;
+DROP TABLE IF EXISTS photos;
 DROP TABLE IF EXISTS restaurants;
 
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY,
   business_name TEXT NOT NULL,
+  -- optimize later by not makiung unique, but passing in int
   place_id SERIAL UNIQUE NOT NULL,
   google_rating REAL NOT NULL,
   zagat_food_rating REAL NOT NULL,
@@ -13,13 +15,17 @@ CREATE TABLE restaurants (
   price_level SMALLINT NOT NULL,
   business_type TEXT NOT NULL,
   longitude REAL NOT NULL,
-  latitude REAL NOT NULL,
-  photos TEXT[] NOT NULL
+  latitude REAL NOT NULL
 );
 
 CREATE TABLE nearby (
   place_id INTEGER NOT NULL,
   recommended INTEGER NOT NULL
+);
+
+CREATE TABLE photos (
+  place_id INTEGER NOT NULL,
+  photo_url VARCHAR(2084)
 );
 
 -- terminal command to run the sql file, naming which database to do the work in

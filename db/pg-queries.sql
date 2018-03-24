822a1eb7b3befaa62bb459a8a40661bc2659f199
Henry Yang <henryyang@MVPMBP.local>
2018-03-24 16:48:20 -0700

    Move

--- db/pg-queries.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM restaurants INNER JOIN nearby ON restaurants.place_id = nearby.recommended WHERE nearby.place_id = 1;
--- don't use *, be specific with fields- restaurants.longitude
-
-SELECT * FROM restaurants INNER JOIN nearby ON restaurants.place_id = nearby.recommended INNER JOIN photos on photos.place_id = nearby.recommended WHERE nearby.place_id = 1;

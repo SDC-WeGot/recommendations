03a0e4f4a135389efc1dfc95e7908def7c674505
Henry Yang <contacthenryyang@gmail.com>
2018-03-23 17:31:28 -0700

    Write add foreign key script two table version

--- db/createFK.sql
+++ db/createFK.sql
@@ -1,6 +1,15 @@
 ALTER TABLE nearby
-ADD CONSTRAINT place_id
-FOREIGN KEY (place_id) REFERENCES restaurants;
+ADD CONSTRAINT fk_nearby_placeid_placeid
+FOREIGN KEY (place_id) REFERENCES restaurants (place_id);
+
 ALTER TABLE nearby
-ADD CONSTRAINT recommended
-FOREIGN KEY (place_id) REFERENCES restaurants;
+ADD CONSTRAINT fk_nearby_recommended_placeid
+FOREIGN KEY (recommended) REFERENCES restaurants (place_id);
+
+
+ALTER TABLE photos
+ADD CONSTRAINT fk_photos_placeid_placeid
+FOREIGN KEY (place_id) REFERENCES restaurants (place_id);
+
+-- 'ALTER TABLE nearby ADD CONSTRAINT fk_nearby_placeid_placeid FOREIGN KEY (place_id) REFERENCES restaurants (place_id); ALTER TABLE nearby ADD CONSTRAINT fk_nearby_recommended_placeid FOREIGN KEY (recommended) REFERENCES restaurants (place_id); ALTER TABLE photos ADD CONSTRAINT fk_photos_placeid_placeid FOREIGN KEY (place_id) REFERENCES restaurants (place_id);'
+

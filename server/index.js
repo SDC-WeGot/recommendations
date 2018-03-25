f5f1bfc0a3699aeb24b1cebe3abed0269ed50654
Henry Yang <henryyang@MVPMBP.local>
2018-03-25 13:14:52 -0700

    Configure MS server to query PG

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const dbController = require('../db/pg-queries');
+const pg = require('pg');
 
-var restaurants = require('../db/models/restaurant.js');
-var mongoose = require('mongoose');
 const dbAddress = process.env.DB_ADDRESS || 'localhost';
+var connectionString = `postgres://${dbAddress}:5432/sagat_sql`;
+var pgClient = new pg.Client(connectionString);
+pgClient.connect();
 
-var uri = `mongodb://${dbAddress}/wegot`;
-mongoose.connect(uri, { useMongoClient: true });
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,31 +24,13 @@ app.get('/api/restaurants/:id/recommendations', function (req, res) {
   console.log("GET " + req.url);
   // find recommended restaurants based on id
   var results = [];
-  restaurants.findOne(placeId, (err, data)=> {
-    if(err){
+  dbController.queryPG(placeId, (err, data) => {
+    if (err, null) {
       res.status(500);
-      console.log(err);
-    } else{
-      // console.log("restaurant info:",data);
-      // console.log("data: ",data);
-      var nearbyArr = data[0].nearby;
-      // console.log(nearbyArr);
-      results.push(data[0]);
-
-      restaurants.findMany(nearbyArr, (err, data)=> {
-        if(err){
-          res.status(500);
-          console.log(err);
-        } else{
-          // console.log("recommended restaurants:", data);
-          results.push(data)
-          // console.log("number of recommended: " + data.length);
-          res.status(200);
-          // res.send(data);
-          // console.log(results.length);
-          res.send(results);
-        }
-      });
+      console.log(`Error: ${err}`);
+    } else if (null, data) {
+      res.status(200);
+      res.send(data);
     }
   });
 });

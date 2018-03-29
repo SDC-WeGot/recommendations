67dc2d992f76f821b34af6ae1457a80b8e4d0c61
Henry Yang <contacthenryyang@gmail.com>
2018-03-28 21:32:54 -0700

    Reconfigure so MS does SSR; next need to configure proxy for SSR

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const webpack = require('webpack');
+const path = require('path');
 
-module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
-  module : {
-    loaders : [
+const common = {
+  context: __dirname + '/client/src',
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
-       }
+          presets: ['react', 'es2015', 'env']
+        },
       },
       {
         test: /\.css$/,
         use: ['style-loader','css-loader']
       }
-    ]
+    ],
   }
 };
+
+const client = {
+  entry: './client.js',
+  output: {
+    path: __dirname + '/client/dist',
+    filename: 'app.js'
+  }
+};
+
+const server = {
+  entry: './server.js',
+  target: 'node',
+  output: {
+    path: __dirname + '/client/dist',
+    filename: 'app-server.js',
+    libraryTarget: 'commonjs-module'
+  }
+};
+
+module.exports = [
+  Object.assign({}, common, client),
+  Object.assign({}, common, server)
+];

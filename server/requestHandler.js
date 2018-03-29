81fc8b2f3fba98264618baaf473ecc87f37695c8
Henry Yang <contacthenryyang@gmail.com>
2018-03-28 17:44:55 -0700

    Create handler that checks redis, and only checks pg if not in redis

---
+++ server/requestHandler.js
@@ -0,0 +1,24 @@
+const dbController = require('../db/pgpromise-queries');
+
+const Redis = require('ioredis');
+const redis = new Redis();
+
+let handleRequest = async (id) => {
+  let redisResult = await redis.get(id, (err, result) => {
+    if (err) {
+      console.error(err);
+    } else if (result !== null) {
+      console.log('Found cached');
+    }
+  });
+  // console.log('redisResult = ', redisResult);
+  // If not in redis cache, redisResult = null
+  if (!redisResult) {
+    console.log('Looking in postgres');
+    return dbController.queryPG(id);
+  }
+  // Only returns here if redisResult !== null
+  return redisResult;
+};
+
+module.exports = handleRequest;

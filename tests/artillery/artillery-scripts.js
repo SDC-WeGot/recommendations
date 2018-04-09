e461853d947ff6488b1012dbebffeb4d5357a846
Henry Yang <contacthenryyang@gmail.com>
2018-04-08 23:31:47 -0700

    Add realistic web traffic weighting to index generator

--- tests/artillery/artillery-scripts.js
+++ tests/artillery/artillery-scripts.js
@@ -1,8 +1,19 @@
 // Generate a random number for a random product page
-const databaseEntries = 10000000;
+const databaseSize = 10000000;
+
+// Realistic web traffic- 80% going to 50, 15% going to 1000, 5% for the rest
 
 let generateRandomIndex = (userContext, events, done) => {
-  const id = Math.floor(Math.random() * databaseEntries);
+  let randomPercentage = Math.random();
+  let databaseSearchRange;
+  if (randomPercentage <= 0.85) {
+    databaseSearchRange = 50;
+  } else if (randomPercentage > 0.85 && randomPercentage <= 0.95) {
+    databaseSearchRange = 1000;
+  } else {
+    databaseSearchRange = databaseSize;
+  }
+  const id = Math.floor(Math.random() * databaseSearchRange);
   userContext.vars.id = id;
   return done();
 };
